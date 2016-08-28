152fea33fe4f302a228f879e7a9d38f53f2a51af
Barak Tom <karbaraka@gmail.com>
2016-08-28 20:51:28 -0300

    Adding C++ & Java Invoice Classes

---
+++ How_To_Program_Exercises/JavaHowToProgram9/Chapter3/InvoiceClass/Invoice.java
@@ -0,0 +1,21 @@
+//
+//	Invoice Class 
+//
+
+class Invoice
+{
+	private String number;
+	private String description;
+	private int quantity;
+	private double pricePerItem;
+	
+	public Invoice (String n, String d, int q, double p)
+	{
+		number = n;
+		description = d;
+		quantity = q;
+		pricePerItem = p;
+	}
+	
+	
+}
